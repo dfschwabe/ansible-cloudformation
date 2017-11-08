9268d523bed37917c90eb3fae09afbbf16011241
User <user@Users-MacBook-Pro.local>
2017-11-08 00:01:14 -0600

    split out base API for multiple resources (yaml!)

--- go.sh
+++ go.sh
@@ -1,6 +1,1 @@
-export ANSIBLE_VAULT_PASSWORD_FILE=.vault_pass
-
-zip hello.zip handler.js
-aws s3 cp hello.zip s3://dans-bukkit/dev/hello/
-ansible localhost -m template -a "src=template.json.j2 dest=./template.json" -e @vars.yaml -e "stage=dev"
-aws cloudformation create-stack --stack-name hello --capabilities CAPABILITY_NAMED_IAM --template-body file://template.json
+aws cloudformation update-stack --stack-name hello --capabilities CAPABILITY_NAMED_IAM --template-body file://template.yml
